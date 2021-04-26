125bfe98f12a0e4f196518192648251842afef79
Stefano Sanvito <stefano.sanvito@promo.it>
2021-04-26 15:43:17 +0200

    removed ssh connection: only sftp allowed

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
 # avoid missing permissions error to open
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
-echo '🏎 establishing ssh connection...'
-ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
-echo '🏁 ssh connection established'
+# echo '🏎 establishing ssh connection...'
+# ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+# echo '🏁 ssh connection established'
 
 echo '🏎 deploying to sftp'
 # create a temporary file containing sftp commands
